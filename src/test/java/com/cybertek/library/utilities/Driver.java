b8fdb594840c86f3068d2fd3420860a2cbe448d0
sobirjon001 <sobirjon001@gmail.com>
2021-04-13 19:20:25 -0400

    fixe web driver

--- src/test/java/com/cybertek/library/utilities/Driver.java
+++ src/test/java/com/cybertek/library/utilities/Driver.java
@@ -18,7 +18,7 @@ public class Driver {
     if(driver == null){
 
         try {
-          URL url = new URL("http://192.168.1.179:4444/wd/hub");
+          URL url = new URL("http://host:4444/wd/hub");
           DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
           desiredCapabilities.setBrowserName("chrome");
           driver = new RemoteWebDriver(url, desiredCapabilities);
